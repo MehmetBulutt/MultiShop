c344d044824ddeb844499dd49f3930e5d69b9158
MehmetBulutt <mehmetbulut7176@gmail.com>
2025-04-28 00:20:11 +0300

    CQRS mimarisi ile order servisi oluşturuldu.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressByIdQueryHandler
+    {
+        private readonly IRepository<Address> _Repository;
+        public GetAddressByIdQueryHandler(IRepository<Address> repository)
+        {
+            _Repository = repository;
+        }
+        public async Task<GetAddressByIdQueryResult> Handle (GetAddressByIdQueryResult query)
+        {
+            var values = await _Repository.GetByIdAsync(query.AddressId);
+            return new GetAddressByIdQueryResult
+            {
+                AddressId = values.AddressId,
+                City = values.City,
+                Detail = values.Detail,
+                District = values.District,
+                UserId = values.UserId,
+            };
+        }
+    }
+}
